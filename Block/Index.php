332ff1c423ad5bda56af752cc1a537b3cf722c79
ThanhNguyen110 <thanhnv.hn110@gmail.com>
2021-06-24 11:42:52 +0700

    first upload

---
+++ Block/Index.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AHT\FrontendTest\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class Index extends Template
+{
+}
---
+++ requirejs-config.js
@@ -0,0 +1,5 @@
+var config = {
+    paths:{
+        frontendtest:"AHT_FrontendTest/view/frontend/templates/index"
+    }
+}
